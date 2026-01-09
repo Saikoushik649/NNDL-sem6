0ab375335a7a32f7a083736532e67746e7d352a8
Saikoushik649 <ksailebtech24@ced.alliance.edu.in>
2026-01-09 10:23:50 +0530

    Create  Spam Classifier.py

---
+++ Spam Classifier.py
@@ -0,0 +1,25 @@
+import math
+
+free = 0
+win = 1
+often = 1
+
+
+h1 = free * 0.5
+h1 = h1 + win * (-0.2)
+h1 = h1 + often * 0.3
+h1 = max(0, h1)
+
+h2 = free * 0.4
+h2 = h2 + win * 0.1
+h2 = h2 + often * (-0.5)
+h2 = max(0, h2)
+
+
+out = (h1 * 0.7) + (h2 * 0.2)
+spam = 1 / (1 + math.exp(-out))
+
+print("n1 =", h1)
+print("n2 =", h2)
+print("Raw output =", out)
+print("Spam probability =", spam)
